ff5c1eded421cdcae7073d3fcbd45298c176bbd8
escohen3 <escohen.us@gmail.com>
2025-07-23 14:15:29 -0500

    Add files via upload

---
+++ relay-config.js
@@ -0,0 +1,4 @@
+export const FILESTACK_API_KEY = 'ACn4cLzxLRWSJd0sekWsVz';
+export const KEY_LIST_URL = 'https://escohen3.github.io/relay-auth/relay-keys.json';
+export const MASTER_KEY = 'RH-ALPHA-1887';
+export const GPT_PROXY_URL = 'http://localhost:5001/gpt-text';
